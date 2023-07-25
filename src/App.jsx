c438fa034f9c607eed5de30a0e783a24b38f15a7
nik <nnaiker01@gmail.com>
2023-07-26 00:35:00 +1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" mb-5 mt-10 text-center text-5xl font-bold text-text  underline">
+        hi
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    colors: {
+      text: "rgb(5, 4, 11)",
+      background: "rgb(243, 240, 255)",
+      primary: "rgb(52, 30, 178)",
+      secondary: "rgb(210, 210, 239)",
+      accent: "rgb(54, 36, 155)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
